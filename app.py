5cf563f2725c3946d92585886b9fc6cd7af8519d
paul <pramirez@unitru.edu.pe>
2024-08-25 19:48:50 -0500

    Se agregan comentarios

--- app.py
+++ app.py
@@ -1,42 +1,52 @@
 import streamlit as st
 import pandas as pd
 from logica import roomies_compatibles
-from componentes import  compatibilidad,roomies_seed, tabla_compatibilidad
+from componentes import compatibilidad, roomies_seed, tabla_compatibilidad
 
+# Configuración de la página de Streamlit
 st.set_page_config(layout="wide")
-resultado=None
+resultado = None
 
-st.markdown(f'<div style="margin-top: 60px;"></div>', unsafe_allow_html=True)
+# Espaciado al principio de la página
+st.markdown('<div style="margin-top: 60px;"></div>', unsafe_allow_html=True)
+
+# Barra lateral de la aplicación
 with st.sidebar:
-  st.header("PERFECT ROOMMATE")
-  roomies1 = st.text_input('Roomie 1')
-  roomies2 = st.text_input('Roomie 2')
-  roomies3 = st.text_input('Roomie 3')
-  
-  num_companeros = st.text_input("Cuantos inquilinos son?")
-  
-  
-  if st.button('Buscar nuevos companeros'):
-    try:
-      topn=int(num_companeros)
-    except ValueError:
-      st.error('Ingrese valor valido')
-      topn=None
-      
-    id_roomies = roomies_seed(roomies1,roomies2,roomies3,topn)
-    if id_roomies and topn is not None:
-      resultado= roomies_compatibles(id_roomies,topn)
-if isinstance(resultado,str):
-  st.error(resultado)
+    st.header("PERFECT ROOMMATE")
+    
+    # Entradas de texto para los roomies
+    roomies1 = st.text_input('Roomie 1')
+    roomies2 = st.text_input('Roomie 2')
+    roomies3 = st.text_input('Roomie 3')
+    
+    # Entrada para el número de compañeros
+    num_companeros = st.text_input("Cuántos inquilinos son?")
+    
+    # Botón para buscar nuevos compañeros
+    if st.button('Buscar nuevos compañeros'):
+        try:
+            topn = int(num_companeros)
+        except ValueError:
+            st.error('Ingrese un valor válido para el número de compañeros')
+            topn = None
+        
+        # Generar IDs de roomies y buscar compatibilidad
+        id_roomies = roomies_seed(roomies1, roomies2, roomies3, topn)
+        if id_roomies and topn is not None:
+            resultado = roomies_compatibles(id_roomies, topn)
+
+# Mostrar los resultados
+if isinstance(resultado, str):
+    st.error(resultado)
 elif resultado is not None:
-  cols = st.columns((1,2))
-  
-  with cols[0]:
-    st.write("Nivel de compatibilida de cada compañero nuevo")
-    fig_grafico=compatibilidad(resultado[1])
-    st.pyplot(fig_grafico)
-  with cols[1]:
-    st.write("Comparativa entre compañeros")
-    fig_tabla=tabla_compatibilidad(resultado)
-    st.plotly_chart(fig_tabla,use_container_width=True)
-   
+    cols = st.columns((1, 2))
+    
+    with cols[0]:
+        st.write("Nivel de compatibilidad de cada compañero nuevo")
+        fig_grafico = compatibilidad(resultado[1])
+        st.pyplot(fig_grafico)
+    
+    with cols[1]:
+        st.write("Comparativa entre compañeros")
+        fig_tabla = tabla_compatibilidad(resultado)
+        st.plotly_chart(fig_tabla, use_container_width=True)
--- componentes.py
+++ componentes.py
@@ -3,62 +3,63 @@ import seaborn as sns
 import plotly.graph_objs as go
 import streamlit as st
 
-# TABLA DE COMPATIBILIDAD
+# FUNCION PARA GENERAR LA GRAFICA DE COMPATIBILIDAD
 def compatibilidad(compatib):
-  compatib /= 100                         #Escala 1 a 0
-  fig, ax = plt.subplots(figsize=(5,4))   #Datos para la grafica
-  sns.barplot(x=compatib.index, y=compatib.values, ax=ax, color='blue')   #Grafica de barras
-  sns.despine(top=True,right=True,left=True,bottom=True)     #Elimina las lineas del grafico
-  
-  # Etiquetas configuracion 
-  ax.set_xlabel('ID roomie', fontsize=10) #Etiqueta eje X
-  ax.set_ylabel('Compatibility(%)', fontsize=10) # Etiqueta eje Y
-  ax.set_xticklabels(ax.get_xticklabels(), rotation=45) #Rotacion de las etiquetas
-  # Ajustar etiqueta y con porcentajes
-  ax.set_yticklabels(['{:.1f}%'.format(y*100) for y in ax.get_yticks()], fontsize=9)  
-  # Etiqueta de cada barra
-  for p in ax.patches:
-    height = p.get_height()
-    ax.annotate('{:.1f}%'.format(height*100),
-                (p.get_x()+p.get_width()/2., height),
-                ha='center', va='center', 
-                xytext=(0, 5),
-                textcoords='offset points', fontsize=8)
-  return (fig)
+    compatib /= 100  # Escalar de 1 a 0
+    fig, ax = plt.subplots(figsize=(5, 4))  # Crear una figura y un eje para la gráfica
+    sns.barplot(x=compatib.index, y=compatib.values, ax=ax, color='blue')  # Crear gráfica de barras
+    sns.despine(top=True, right=True, left=True, bottom=True)  # Eliminar las líneas del gráfico
+    
+    # Configuración de etiquetas
+    ax.set_xlabel('ID roomie', fontsize=10)  # Etiqueta del eje X
+    ax.set_ylabel('Compatibility (%)', fontsize=10)  # Etiqueta del eje Y
+    ax.set_xticklabels(ax.get_xticklabels(), rotation=45)  # Rotación de las etiquetas del eje X
+    
+    # Ajustar etiqueta y con porcentajes
+    ax.set_yticklabels(['{:.1f}%'.format(y * 100) for y in ax.get_yticks()], fontsize=9)
+    
+    # Etiquetas en cada barra
+    for p in ax.patches:
+        height = p.get_height()
+        ax.annotate('{:.1f}%'.format(height * 100),
+                    (p.get_x() + p.get_width() / 2., height),
+                    ha='center', va='center',
+                    xytext=(0, 5),
+                    textcoords='offset points', fontsize=8)
+    return fig
 
-#GENERAR TABLA DE ROOMIES
+# FUNCION PARA GENERAR LA TABLA DE COMPATIBILIDAD
 def tabla_compatibilidad(resultado):
-  resultado_0_with_index=resultado[0].reset_index()  #Convierte el indice en una columna
-  resultado_0_with_index.rename(columns={'index':'Atributo'}, inplace=True) #Renombra la columna
-  
-  #Configuracion grafica
-  fig_table=go.Figure(data=[go.Table(
-    columnwidth=[20]+[10]*(len(resultado_0_with_index.columns)-1), #Ancho de las columnas
-    header=dict(values=list(resultado_0_with_index.columns), #Nombres de las columnas
-                fill_color='paleturquoise',#Color de relleno
-                align='left'),  #Alineamiento y tipo de letra                 
-          cells=dict(values=[resultado_0_with_index[col] for col in resultado_0_with_index.columns],
+    resultado_0_with_index = resultado[0].reset_index()  # Convierte el índice en una columna
+    resultado_0_with_index.rename(columns={'index': 'Atributo'}, inplace=True)  # Renombrar la columna
+    
+    # Configuración gráfica
+    fig_table = go.Figure(data=[go.Table(
+        columnwidth=[20] + [10] * (len(resultado_0_with_index.columns) - 1),  # Ancho de las columnas
+        header=dict(values=list(resultado_0_with_index.columns),  # Nombres de las columnas
+                    fill_color='paleturquoise',  # Color de relleno
+                    align='left'),  # Alineamiento y tipo de letra                 
+        cells=dict(values=[resultado_0_with_index[col] for col in resultado_0_with_index.columns],
                    fill_color='lavender',
                    align='left'))
-  ])
-  
- #Configuracion grafica
-  fig_table.update_layout(
-    width=700, height=320,  #Ancho y alto de la grafica
-    margin=dict(l=0, r=0, t=0, b=0)
-  )
-  return(fig_table)
-
-#ROOMIES SEMILLA
-def roomies_seed(roomies1,roomies2,roomies3,topn):
-  #Crear lista de identificadores  inquilinos ingresados a INT
-  id_roomies =[]
-  for roomie in [roomies1,roomies2,roomies3]:
-    try :
-      if roomie:
-        id_roomies.append(int(roomie))
+    ])
     
-    except ValueError:
-      st.error(f'Se requieren `{roomie}`no es un numero valido')
-      id_roomies=[]
-  return (id_roomies)
+    # Configuración de la tabla
+    fig_table.update_layout(
+        width=700, height=320,  # Ancho y alto de la tabla
+        margin=dict(l=0, r=0, t=0, b=0)  # Márgenes
+    )
+    return fig_table
+
+# FUNCION PARA VALIDAR Y DEVOLVER IDs DE ROOMIES
+def roomies_seed(roomies1, roomies2, roomies3, topn):
+    # Crear lista de identificadores de inquilinos ingresados a INT
+    id_roomies = []
+    for roomie in [roomies1, roomies2, roomies3]:
+        try:
+            if roomie:
+                id_roomies.append(int(roomie))
+        except ValueError:
+            st.error(f'Se requiere que `{roomie}` sea un número válido')
+            id_roomies = []  # Reiniciar lista en caso de error
+    return id_roomies
